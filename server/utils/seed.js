f12a58a6c5d7e89b4a6088ff53e3b63cb1b79c46
Christian.Graceffa <christiamgraceffa@gmail.com>
2025-10-26 12:35:11 -0400

    Read one csv file

--- server/utils/seed.js
+++ server/utils/seed.js
@@ -1,12 +1,28 @@
 //Will be used to populate the database
+//This website was used to parse csv https://csv.js.org/parse/api/sync/ with the options https://csv.js.org/parse/options/
 import {db} from '../db/db.js';
 import fs from 'fs/promises'
-
+import { parse } from "csv-parse/sync"
 try {
   // TODO replace cluster0 with your db name
-  await db.connect('test');
+  
+  await db.connect('bagdb');
   // TODO set the collection
-  await db.setCollection("quote");
+  await db.setCollection("headlines");
+
+  //will read all headlines
+  
+  //------------ONE CSV FILE READ--(cnbc)-----READ THROUGH my comments to understand--------------
+  const fileContent = await fs.readFile('./data/cnbc_headlines.csv', 'utf8')
+  const records = parse(fileContent, {
+  columns: true, //This makes it use the first line as headers
+  skip_empty_lines: true, //Skips empty line
+  delimiter: ',' //Coloumn separator
+});
+// use to test result
+  console.log(records);
+//--------------END OF THE ONE CSV FILE READ-----------------------
+
 } catch (e) {
   console.error('could not seed');
   console.dir(e);
