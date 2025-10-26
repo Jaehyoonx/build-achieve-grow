aca7b7a7e6ec6d8899002ecfc13348cfccf8fcaa
Christian.Graceffa <christiamgraceffa@gmail.com>
2025-10-26 13:24:52 -0400

    Add All Headlines to db

--- server/utils/seed.js
+++ server/utils/seed.js
@@ -3,6 +3,7 @@
 import {db} from '../db/db.js';
 import fs from 'fs/promises'
 import { parse } from "csv-parse/sync"
+import { fileURLToPath } from 'url';
 try {
   // TODO replace cluster0 with your db name
   
@@ -11,17 +12,26 @@ try {
   await db.setCollection("headlines");
 
   //will read all headlines
-  
-  //------------ONE CSV FILE READ--(cnbc)-----READ THROUGH my comments to understand--------------
-  const fileContent = await fs.readFile('./data/cnbc_headlines.csv', 'utf8')
-  const records = parse(fileContent, {      
-  columns: true, //This makes it use the first line as headers
-  skip_empty_lines: true, //Skips empty line
-  delimiter: ',' //Coloumn separator
-});
+  const allHeadlines = ['cnbc_headlines.csv', 'guardian_headlines.csv', 'reuters_headlines.csv']
+  //------------Many CSV FILE READ--(headLines ONLY)-----READ THROUGH my comments to understand--------------
+  let allResults = []
+
+  for(const filename of allHeadlines){
+    const fileContent = await fs.readFile(`./data/${filename}`, `utf8`)
+    const records = parse(fileContent, {      
+      columns: true, //This makes it use the first line as headers
+      skip_empty_lines: true, //Skips empty line
+      delimiter: ',' //Coloumn separator
+    });
+    //Ryan and Haider if this does not makes sense message me
+    //Spead cause mongo expects [{}, {}] while without the ... it does [[{}], [{}]]
+    //... makes sure it does what monogdb accepts which pretty much is a flat array
+    allResults.push(...records);
+}
+ await db.collection.insertMany(allResults);
 // use to test result
   console.log(records);
-//--------------END OF THE ONE CSV FILE READ-----------------------
+//--------------END OF THE Headlines CSV FILE READ-----------------------
 
 } catch (e) {
   console.error('could not seed');
