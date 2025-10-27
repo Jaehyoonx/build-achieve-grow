b742caea2416f198be5e3456cc0664003f1154ad
Haider Ahmed <haider.ahmed@dawsoncollege.qc.ca>
2025-10-26 20:49:37 -0400

    split app and startup into api.js and bin/www

--- server/api.js
+++ server/api.js
@@ -3,31 +3,17 @@ import { db } from './db/db.js';
 import headlineRoute from './routes/headlines.js';
 import etfsRoute from './routes/etfs.js';
 import stockRoute from './routes/stocks.js';
+
 const app = express();
 const port = 3000;
 
 app.use(express.static('../client/dist'));
 
-async function startServer(){
-  await db.connect('bagdb');
+await db.connect('bagdb');
 
-  app.use('/api', headlineRoute);
-  app.use('/api', etfsRoute);
-  app.use('/api', stockRoute);
-  try {
-    const server = app.listen(port, () => {
-      console.error(`Example app app listening at http://localhost:${port}`);
-    });
-    //SIGTERM doesn't work on Windows. This works when the server is a Unix process and we `kill` it
-    process.on('SIGTERM', () => {
-      console.error('SIGTERM signal received: closing HTTP server');
-      server.close(() => {
-        console.error('HTTP server closed');
-      });
-    });
-  } catch (err) {
-    console.error('Error while starting', err.message);
-    process.exit(1);
-  }
-}
-startServer();
+app.use('/api', headlineRoute);
+app.use('/api', etfsRoute);
+app.use('/api', stockRoute);
+
+export default app;
+export { port };
