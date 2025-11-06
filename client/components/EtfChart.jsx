01e09ee61989d7d2e77e5c38898ca248d1e299e6
Haider Ahmed <haider.ahmed@dawsoncollege.qc.ca>
2025-11-06 18:54:16 -0500

    add EtfChart component skeleton

---
+++ client/components/EtfChart.jsx
@@ -0,0 +1,6 @@
+// Component: EtfChart
+// Purpose: Placeholder for ETF price history chart.
+// Will visualize historical ETF price data.
+export default function EtfChart() {
+  return <div></div>;
+}
