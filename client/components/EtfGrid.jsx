7d13ee006f803e5e3a4ca1b33b967ed470930ba5
Haider Ahmed <haider.ahmed@dawsoncollege.qc.ca>
2025-11-06 18:54:22 -0500

    add EtfGrid component skeleton

---
+++ client/components/EtfGrid.jsx
@@ -0,0 +1,6 @@
+// Component: EtfGrid
+// Purpose: Displays a grid/list of ETF cards.
+// Users can browse popular ETFs and select one for more details.
+export default function EtfGrid() {
+  return <div></div>;
+}
