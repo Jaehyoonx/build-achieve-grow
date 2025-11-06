65930bacd290d9f49f2a1e38b57ea0000bf6cc8b
Haider Ahmed <haider.ahmed@dawsoncollege.qc.ca>
2025-11-06 18:54:02 -0500

    add CompareChart component skeleton

---
+++ client/components/CompareChart.jsx
@@ -0,0 +1,6 @@
+// Component: CompareChart
+// Purpose: Placeholder for a chart that compares two selected stocks or ETFs.
+// Will render a multi-line visualization (e.g., Plotly).
+export default function CompareChart() {
+  return <div></div>;
+}
