426e30e93756265f04b5b0780fb08a18d91b4e46
Haider Ahmed <haider.ahmed@dawsoncollege.qc.ca>
2025-11-06 18:54:28 -0500

    add NewsFeed component skeleton

---
+++ client/components/NewsFeed.jsx
@@ -0,0 +1,6 @@
+// Component: NewsFeed
+// Purpose: Shows news headlines relevant to a selected stock/ETF.
+// Will link financial events to price movements.
+export default function NewsFeed() {
+  return <div></div>;
+}
