41a9c56f92e5048f7acf46cbd57fff81a88b5f26
Haider Ahmed <haider.ahmed@dawsoncollege.qc.ca>
2025-11-06 18:54:10 -0500

    add EtfCard component skeleton

---
+++ client/components/EtfCard.jsx
@@ -0,0 +1,6 @@
+// Component: EtfCard
+// Purpose: Represents a single ETF item in a grid/list.
+// Will display ETF ticker, price, and basic movement indicator (green/red).
+export default function EtfCard() {
+  return <div></div>;
+}
