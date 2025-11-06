6bc24a11c5349b4a821f1964e6b064873c006b82
Haider Ahmed <haider.ahmed@dawsoncollege.qc.ca>
2025-11-06 18:54:39 -0500

    add StockCard component skeleton

---
+++ client/components/StockCard.jsx
@@ -0,0 +1,6 @@
+// Component: StockCard
+// Purpose: Represents a single stock in a grid/list.
+// Displays ticker, current price, and up/down color indicator.
+export default function StockCard() {
+  return <div></div>;
+}
