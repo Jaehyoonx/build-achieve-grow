6ca324a8dab02e102885c857d4ef27a5bf3ceed3
Haider Ahmed <haider.ahmed@dawsoncollege.qc.ca>
2025-11-06 18:54:44 -0500

    add StockChart component skeleton

---
+++ client/components/StockChart.jsx
@@ -0,0 +1,6 @@
+// Component: StockChart
+// Purpose: Placeholder for stock price history chart.
+// Will visualize stock price movements over time (e.g., Plotly).
+export default function StockChart() {
+  return <div></div>;
+}
