4bf7fc538a05cfcea411430c06a197dd5d5c4c00
Christian.Graceffa <christiamgraceffa@gmail.com>
2025-11-10 13:42:40 -0500

    Add Filter for HeadlineList for Year

--- client/src/components/NewsFeed.jsx
+++ client/src/components/NewsFeed.jsx
@@ -1,6 +1,6 @@
 // Component: NewsFeed
 // Purpose: Shows news headlines relevant to a selected stock/ETF.
-// Will link financial events to price movements.
+// Will link The year to the related stock
 export default function NewsFeed() {
   return <div></div>;
 }
