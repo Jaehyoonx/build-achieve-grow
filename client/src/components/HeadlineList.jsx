a194d4d280ff77c09bf7cc0daacf0b7759fdf5a5
Christian.Graceffa <christiamgraceffa@gmail.com>
2025-11-10 13:42:40 -0500

    Add Filter for HeadlineList for Year

--- client/src/components/HeadlineList.jsx
+++ client/src/components/HeadlineList.jsx
@@ -1,17 +1,30 @@
+//This Headline list is used for the filter of the headlines based on the years
 import { useEffect, useState } from 'react';
 
-export default function HeadlineList() {
+export default function HeadlineList({ year }) {
   const [headlines, setHeadlines] = useState([]);
-
+  const [loading, setLoading] = useState(true);
+ 
   useEffect(() => {
     const fetchHeadlines = async () => {
       try {
         const res = await fetch('/api/headlines');
         if (!res.ok) throw new Error('Network response was not ok');
         const data = await res.json();
-        setHeadlines(data);
+        
+        //Just in case its a little complicated with the regex
+        //This regex pattern just works as followed it gets digites from 0-9
+        //gets it {4} times
+        //Has to match exactly 4 digits
+        const filetered = data.fetch(desired => {
+          const desiredYear = desired.Time.match(/\d{4}/)[0];
+          return desiredYear === year;
+        })
+        setHeadlines(filetered);
       } catch (err) {
         console.error('Error fetching headlines:', err);
+      } finally {
+        setLoading(false);
       }
     };
 
