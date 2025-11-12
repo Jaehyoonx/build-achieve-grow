674d151978bc353e0544c08a0d31b54433403760
Ryan Bui <ryan.bui@dawsoncollege.qc.ca>
2025-11-11 22:37:29 -0500

    Add StockGrid to app

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,12 @@
 import './App.css';
-import NewsPage from './views/NewsPage';
+// import NewsPage from './views/NewsPage';
+import StockGrid from './components/StockGrid';
 function App() {
   return (
     <div>
       <h1>B.A.G. Client</h1>
       
-      <NewsPage />
+      <StockGrid />
     </div>
   );
 }
