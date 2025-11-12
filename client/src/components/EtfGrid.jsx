865b3ffb437a5e24f90ac23768836a9d3aab4283
Haider Ahmed <haider.ahmed@dawsoncollege.qc.ca>
2025-11-11 20:26:30 -0500

    make EtfGrid

--- client/src/components/EtfGrid.jsx
+++ client/src/components/EtfGrid.jsx
@@ -1,6 +1,34 @@
 // Component: EtfGrid
 // Purpose: Displays a grid/list of ETF cards.
 // Users can browse popular ETFs and select one for more details.
-export default function EtfGrid() {
-  return <div></div>;
+import { useState } from 'react';
+import EtfCard from './EtfCard';
+import EtfDetail from '../views/EtfDetail';
+
+export default function EtfGrid({ etfs }) {
+  const [selectedSymbol, setSelectedSymbol] = useState(null);
+
+  if (selectedSymbol) {
+    return (
+      <EtfDetail 
+        symbol={selectedSymbol}
+        onBack={() => setSelectedSymbol(null)}
+      />
+    );
+  }
+
+  return (
+    <div className="etf-grid">
+      {etfs.map(etf => (
+        <EtfCard
+          key={etf.symbol}
+          symbol={etf.symbol}
+          etfName={etf.name}
+          currentPrice={etf.currentPrice}
+          previousClose={etf.previousClose}
+          onClick={() => setSelectedSymbol(etf.symbol)}
+        />
+      ))}
+    </div>
+  );
 }
