b93b3d41496a93405e93bcbc323601dcc0695cb4
Haider Ahmed <haider.ahmed@dawsoncollege.qc.ca>
2025-11-15 22:21:52 -0500

    add PriceChart component and refactor Etf + Stock chart

---
+++ client/src/components/etf/EtfChart.jsx
@@ -0,0 +1,9 @@
+// Component: EtfChart
+// Purpose: Placeholder for ETF price history chart.
+// Will visualize ETF price movements over time (e.g., Recharts).
+
+import PriceChart from "../shared/PriceChart";
+
+export default function EtfChart(props) {
+  return <PriceChart {...props} />;
+}
---
+++ client/src/components/stock/StockChart.jsx
@@ -0,0 +1,9 @@
+// Component: StockChart
+// Purpose: Placeholder for stock price history chart.
+// Will visualize stock price movements over time (e.g., Recharts).
+
+import PriceChart from "../shared/PriceChart";
+
+export default function StockChart(props) {
+  return <PriceChart {...props} />;
+}
